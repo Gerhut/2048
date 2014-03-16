a52ff9af4b1c25078a651ccbc4e839fde19aea7b
George Chung <Gerhut@GMail.com>
2014-03-16 13:56:48 +0800

    make disabledCell outside

--- js/application.js
+++ js/application.js
@@ -1,4 +1,11 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(5, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  var disabledCells = [
+    {x: 0, y: 0},
+    {x: 4, y: 0},
+    {x: 2, y: 2},
+    {x: 0, y: 4},
+    {x: 4, y: 4},
+  ];
+  new GameManager(5, disabledCells, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
--- js/game_manager.js
+++ js/game_manager.js
@@ -1,5 +1,6 @@
-function GameManager(size, InputManager, Actuator, ScoreManager) {
+function GameManager(size, disabledCells, InputManager, Actuator, ScoreManager) {
   this.size         = size; // Size of the grid
+  this.disabledCells= disabledCells;
   this.inputManager = new InputManager;
   this.scoreManager = new ScoreManager;
   this.actuator     = new Actuator;
@@ -35,7 +36,7 @@ GameManager.prototype.isGameTerminated = function () {
 
 // Set up the game
 GameManager.prototype.setup = function () {
-  this.grid        = new Grid(this.size, [{x: 2, y: 2}]);
+  this.grid        = new Grid(this.size, this.disabledCells);
 
   this.score       = 0;
   this.over        = false;
