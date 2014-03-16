f2ed3cd4b749b102b74d068e36813ba5330eac47
Gerhut <Gerhut@GMail.com>
2014-03-16 17:20:01 +0800

    Modify disabled cells.

--- js/application.js
+++ js/application.js
@@ -2,10 +2,13 @@
 window.requestAnimationFrame(function () {
   var disabledCells = [
     {x: 0, y: 0},
-    {x: 4, y: 0},
-    {x: 2, y: 2},
-    {x: 0, y: 4},
-    {x: 4, y: 4},
+    {x: 0, y: 1},
+    {x: 1, y: 0},
+    {x: 1, y: 1},
+    {x: 3, y: 3},
+    {x: 3, y: 4},
+    {x: 4, y: 3},
+    {x: 4, y: 4}
   ];
   new GameManager(5, disabledCells, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
