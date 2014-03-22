a2a6e7cb76f906c3068044aed9be69433c51482a
George Chung <Gerhut@GMail.com>
2014-03-22 16:46:02 +0800

    restore to 4x4 with no disabled tiles

--- js/application.js
+++ js/application.js
@@ -1,14 +1,4 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  var disabledCells = [
-    {x: 0, y: 0},
-    {x: 0, y: 1},
-    {x: 1, y: 0},
-    {x: 1, y: 1},
-    {x: 3, y: 3},
-    {x: 3, y: 4},
-    {x: 4, y: 3},
-    {x: 4, y: 4}
-  ];
-  new GameManager(5, disabledCells, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  new GameManager(4, [], KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
